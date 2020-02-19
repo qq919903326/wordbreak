ebd3762629027162b351e3a979c9e55eed7662bf
hwh <919903326@qq.com>
2020-02-19 11:21:21 +0800

    删除无用内容

--- src/main/java/com/wordbreak/util/GsonProcessor.java
+++ src/main/java/com/wordbreak/util/GsonProcessor.java
@@ -5,21 +5,15 @@ import com.google.gson.Gson;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/**
- * 异步带缓存的线程池
- *
- */
 public class GsonProcessor {
 
-	private GsonProcessor() {
-	}
+	private GsonProcessor() { }
 
 	private static class AsynProcessorHolder {
 		private final static Gson INSTANCE = new Gson();
 	}
 
 	public static Gson getInstance() {
-
 		return AsynProcessorHolder.INSTANCE;
 	}
 }
